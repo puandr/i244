0ea971d820f82bde0fe9c16b3d41ff076e87a163
Andrei Pugatšov <apugatso@itcollege.ee>
2017-02-11 18:30:19 +0200

    PHP works

--- index.php
+++ index.php
@@ -21,11 +21,35 @@
 			$l = mysqli_connect($host, $user, $pass, $db);
 			mysqli_query($l, "SET CHARACTER SET UTF8") or
 					die("Error, ei saa andmebaasi charsetti seatud");
-					
-			
-			$result = mysqli_query($l, "SELECT id, data FROM 10162828_andmed");
-			echo $result->num_rows;
-			
+								
+			$result = mysqli_query($l, "SELECT * FROM 10162828_andmed");
+		
+			$table = '10162828_andmed';
+			$fields_num = mysqli_num_fields($result);
+
+			echo "<h1>Table: {$table}</h1>";
+			echo "<table border='1'><tr>";
+			// printing table headers
+			for($i=0; $i<$fields_num; $i++)
+			{
+				$field = mysqli_fetch_field($result);
+				echo "<td>{$field->name}</td>";
+			}
+			echo "</tr>\n";
+			// printing table rows
+			while($row = mysqli_fetch_row($result))
+			{
+				echo "<tr>";
+
+				// $row is array... foreach( .. ) puts every element
+				// of $row to $cell variable
+				foreach($row as $cell)
+					echo "<td>$cell</td>";
+
+				echo "</tr>\n";
+			}
+			mysqli_free_result($result);			
+						
 			mysqli_close($l);
 		?>
 		
